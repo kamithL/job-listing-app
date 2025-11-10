51acab74bd708f71ef5b55c61e2aaf8cab7650b3
kamithL <kamithlakmal@gmail.com>
2025-11-10 14:24:30 +0530

    update Home page

--- lib/main.dart
+++ lib/main.dart
@@ -13,36 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Scaffold(
+              body: Center(
+                child: Text('Home Page'),
+              ),
+            ),
+        // Add other routes here
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Home Page"),
-      ),
-      body: Center(
- 
-        child:Text('Job Listing App Coming Soon!'),
-      ),
-    );
-  }
-}
